363f22527ac7841aa422055b498ca964aa9a1fce
Jean Santos <>
2022-07-25 15:48:36 +0100

    #JS #HTTPS

--- aula02-stubs/src/services.js
+++ aula02-stubs/src/services.js
@@ -1,8 +1,15 @@
+const http = require ("https")
+const { resolve } = require("path")
+
 class Services {
   async makeRequest(url) {
-    
+    return new Promise((resolve, reject) =>{
+      http.get(url, response => {
+        response.on("data", data => resolve(data))
+        response.on("error", reject)
+      })
+    })
   }
-
 }
 
 
