b648839ed01f5d90898f2f9eaa76d7a67ebf3aa4
Jean Santos <>
2022-11-19 14:32:35 +0000

    demo-type-coercion

--- Modulo02-fundamentos/demo02-type-coercion/index.js
+++ Modulo02-fundamentos/demo02-type-coercion/index.js
@@ -1,21 +1,26 @@
 
 
+// Console.assert compara os valores como o deepStrictEqual
 console.assert(String(123) === "123", "explicit convertion to string")
 
+// A expressão 123 + "" retorna uma string nesse caso está fazendo uma comparação === 
 console.assert(123 + "" === "123", "implicit convertion to string")
 
+// Quando usamos o OU || se os dois forem verdadeiros ele sempre retorna o primeiro
 console.assert("hello" || 1 === "hello", "return always first result of two true")
 
+// Quando usamos o E && caso os dois forem verdadeiros ele sempre retorna o ultimo.
 console.assert("hello" && 123 === 123, "return always first result of two true")
 
 
 const item = {
   name: "Jean Santos",
   age: 30,
-  // String: 
+  // String: 1 se não for primitivo, chama o valueOf
   toString() {
     return `name: ${this.name}, age: ${this.age}`
   },
+  // Number: 1 se não for primitivo, chama o toString
   valueOf() {
     return 007
   },
@@ -27,8 +32,8 @@ const item = {
       string: JSON.stringify(this),
       number: 007
     }
-    return type[coercionType]
-  }
+    return type[coercionType] || type.string 
+  } 
 }
 
 // console.log("String: ", String(item));
