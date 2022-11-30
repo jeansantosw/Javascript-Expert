1f4976fffd1fcfc2c5f7770b4caded3c6a689f8c
Jean Santos <jeansantos.sw@gmail.com>
2022-11-30 15:33:56 +0000

    Internacionalização de dados com correção do path

--- Modulo03-fundamentos-ciclo-de-vida-pt02/demo02-esmodules-internacionalization/src/repository.js
+++ Modulo03-fundamentos-ciclo-de-vida-pt02/demo02-esmodules-internacionalization/src/repository.js
@@ -3,11 +3,7 @@ import { writeFile, readFile } from 'fs/promises'
 export const save = async(data) => {
   // com o modulos não temos o __filename __dirname
   // então passamos a usar dessa forma, porém podemos melhorar
-
   const databaseFile = new URL('./../database.json', import.meta.url)
-  // const filePath = databaseFile.replace("/Users", "Users")
-  // console.log('databaseFile', databaseFile.replace("/Users", "Users"));
-
   const currentData = JSON.parse((await readFile(databaseFile, "utf8")))
   currentData.push(data)
   await writeFile(databaseFile, JSON.stringify(currentData))
