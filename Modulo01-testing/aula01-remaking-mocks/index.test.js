7399101a4781856f5b21fcdca22260848e59d94d
Jean Santos <jeansantos.sw@gmail.com>
2023-04-04 18:58:47 +0100

    trabalhando com mocks

--- Modulo01-testing/aula01-remaking-mocks/index.test.js
+++ Modulo01-testing/aula01-remaking-mocks/index.test.js
@@ -7,6 +7,7 @@ const assert = require("assert")
     // variaveis criadas nesse bloco, só são validas durante sua execução
   {
     const filePath = './mocks/empytFile-invalid.csv'
+    // console.log('filePath: ', filePath);
     const expected = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
     const result = File.csvJSON(filePath)
     await assert.rejects(result, expected)
--- Modulo01-testing/aula01-remaking-mocks/src/file.js
+++ Modulo01-testing/aula01-remaking-mocks/src/file.js
@@ -1,6 +1,9 @@
+const { readFile } = require('fs/promises')
+
 class File {
-  static async csvJSON() {
-    
+  static async csvJSON(filePath) {
+    const result = await readFile(filePath, 'utf8')
+    console.log({result});
   }
 }
 
