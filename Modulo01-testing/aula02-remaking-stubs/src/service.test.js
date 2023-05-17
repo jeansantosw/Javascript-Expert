5fcf21f012b8c656514f9d37ba27303eead84b01
Jean Santos <jeansantos.sw@gmail.com>
2023-05-17 19:34:45 +0100

    trabalhando com mocks

--- Modulo01-testing/aula02-remaking-stubs/src/service.test.js
+++ Modulo01-testing/aula02-remaking-stubs/src/service.test.js
@@ -1,11 +1,29 @@
 const Service = require("./service");
-
+const { createSandbox } = require("sinon")
+const sinon = createSandbox()
 const BASE_URL_1 = "https://swapi.dev/api/planets/1/"
+const BASE_URL_2 = "https://swapi.dev/api/planets/2/"
+
+const mocks = {
+  tatooine: require("../mocks/tatooine.json"),
+  alderaan: require("../mocks/alderaan.json")
+}
+
 
 ;(async () => {
-  {
+// {
+//   vai para internet
+//   const service = new Service()
+//   const dados = await service.makeRequest(BASE_URL_2)
+//   console.log("dados", JSON.stringify(dados));
+// }
+
   const service = new Service()
-  const dados = await service.makeRequest(BASE_URL_1)
-  console.log("dados", dados);
-}
+  sinon.stub(service, service.makeRequest.name)
+  stub.withArgs(BASE_URL_1).resolve(mocks.tatooine)
+
+  {
+
+  }
+
 })()
