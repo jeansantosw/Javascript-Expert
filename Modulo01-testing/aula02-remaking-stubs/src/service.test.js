209596588ca262c83e3ee543f2ac69dc42952cc9
Jean Santos <jeansantos.sw@gmail.com>
2023-05-18 13:30:20 +0100

    trabalhando com mocks

--- Modulo01-testing/aula02-remaking-stubs/src/service.test.js
+++ Modulo01-testing/aula02-remaking-stubs/src/service.test.js
@@ -3,6 +3,7 @@ const { createSandbox } = require("sinon")
 const sinon = createSandbox()
 const BASE_URL_1 = "https://swapi.dev/api/planets/1/"
 const BASE_URL_2 = "https://swapi.dev/api/planets/2/"
+const assert = require("assert")
 
 const mocks = {
   tatooine: require("../mocks/tatooine.json"),
@@ -19,11 +20,27 @@ const mocks = {
 // }
 
   const service = new Service()
-  sinon.stub(service, service.makeRequest.name)
-  stub.withArgs(BASE_URL_1).resolve(mocks.tatooine)
+
+  const stub = sinon.stub(service, service.makeRequest.name)
+
+  stub
+    .withArgs(BASE_URL_1)
+    .resolves(mocks.tatooine)
+
+  stub
+    .withArgs(BASE_URL_2)
+    .resolves(mocks.alderaan)
+
 
   {
+    const expected = {
+      name: "Tatooine",
+      surfaceWater: "1",
+      appeardIn: "5"
+    }
 
+    const results = await service.getPlanets(BASE_URL_1)
+    assert.deepStrictEqual(expected, results)
   }
 
 })()
