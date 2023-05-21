934f99feb69585d544afd1d32c5fff40d0ccde79
Jean Santos <jeansantos.sw@gmail.com>
2023-05-21 12:59:03 +0100

    Finalizando testes coverage

---
+++ Modulo01-testing/aula04-remaking-coverage-e2e/src/api.js
@@ -0,0 +1,24 @@
+const http = require("http")
+
+const routes = {
+  '/contact:get': (request, response) => {
+    response.write('contact us page!')
+    return response.end()
+  },
+  default(request, response) {
+    response.write('Page is not found')
+    return response.end()
+  }
+}
+
+function handle(request, response) {
+  const {url, method} = request
+  const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
+  // console.log(routeKey);
+  // return response.end()
+  const chosen = routes[routeKey] || routes.default
+  return chosen(request, response)
+}
+
+const app = http.createServer(handle).listen(3000, ()=>console.log("running at 3000"))
+
