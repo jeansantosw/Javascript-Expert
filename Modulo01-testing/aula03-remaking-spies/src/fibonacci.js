203d1e0ce0ee35b9911b786006d5127a18fe4b57
Jean Santos <jeansantos.sw@gmail.com>
2023-11-21 21:20:44 +0000

    Aula de spy com generator

--- Modulo01-testing/aula03-remaking-spies/src/fibonacci.js
+++ Modulo01-testing/aula03-remaking-spies/src/fibonacci.js
@@ -1,12 +1,14 @@
 class Fibonacci {
 
   *execute(input, current = 0, next = 1) {
+    // Processou todas as sequencias e para!
     if(input === 0 ) {
       return
     }
-
+    //retorna o valor
     yield current
 
+    //delega a função mas não retorna valor
     yield * this.execute(input - 1, next, next + current)
   }
 
--- Modulo01-testing/aula03-remaking-spies/src/fibonacci.test.js
+++ Modulo01-testing/aula03-remaking-spies/src/fibonacci.test.js
@@ -1,23 +1,33 @@
-const {createSandbox} = require("sinon")
-const Fibonacci = require("./fibonacci")
-const assert = require("assert")
+const { createSandbox } = require('sinon')
+const Fibonacci = require('./fibonacci')
+const assert = require('assert')
 const sinon = createSandbox()
 
-const fibonacci = new Fibonacci()
-
-;(async() =>{
+;(async () => {
   {
-    const spy = sinon.spy(
-      fibonacci,
-      fibonacci.execute.name
-    )
-    for(const sequencia of fibonacci.execute(5)){
+    const fibonacci = new Fibonacci()
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name)
+
+    for (const sequencia of fibonacci.execute(3)) {
     }
+    const expectedCallCount = 4
+    assert.strictEqual(spy.callCount, expectedCallCount)
+  }
+
+  {
+    const fibonacci = new Fibonacci()
+
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name)
+
+    const results = [...fibonacci.execute(5)]
     const expectedCallCount = 6
     assert.strictEqual(spy.callCount, expectedCallCount)
 
     const { args } = spy.getCall(2)
     const expctedParms = [3, 1, 2]
-    assert.deepStrictEqual(args, expctedParms, "Array is not equals")
+    assert.deepStrictEqual(args, expctedParms, 'Array is not equals')
+
+    const expectedResults = [0, 1, 1, 2, 3]
+    assert.deepStrictEqual(results, expectedResults, 'Array is not equals')
   }
 })()
